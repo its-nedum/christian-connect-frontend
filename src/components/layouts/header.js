1ec10900787cc661d699741f478d3d7eb6119e35
its-nedum <de.magnificent1@gmail.com>
2019-12-08 09:47:28 +0100

    features(Added logo image)

--- src/components/layouts/header.js
+++ src/components/layouts/header.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import '../../myStyles/main.css'
-// import logo from '../../image/cc-logo.jpg'
+import logo from '../../image/logo-logo.png'
 
 const Header = () => {
     
     return(
         <div>
-            <Link to='/' className="brand-logo left logo">Christian Connect</Link>
+            <Link to='/' className="brand-logo left logo"><img src={logo} alt="logo" className="responsive-img"/></Link>
             <ul className="right hide-on-small-only">
                 <li><Link to='/'>SignIn</Link></li>
                 <li><Link to='/'>SignUp</Link></li>
