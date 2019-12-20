c0004998464ece428a14594193419bdfc6e0a804
its-nedum <de.magnificent1@gmail.com>
2019-12-20 14:58:42 +0100

    bug(changed community sidebar to Link)

--- src/components/layouts/comSidebar.js
+++ src/components/layouts/comSidebar.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import '../../myStyles/main.css'
+import {Link} from 'react-router-dom'
 
 const ComSidebar = () => {
     return (
         <div className="card">
             <ul className="sidebar-ul">
-                <li><i className="material-icons">dashboard</i><a href="/feed" > Dashboard</a></li>
-                <li><i className="material-icons">supervisor_account</i><a href="/users" > Connect</a></li>
-                <li><i className="material-icons">chat</i><a href="/chat-room"> Chat Rooms</a></li>
-                <li><i className="material-icons">work</i><a href="/job-connect"> Job Alert</a></li>
-                <li><i className="material-icons">event_note</i><a href="/events"> Events</a></li>
-                <li><i className="material-icons">settings</i><a href="/profile"> Profile</a></li>
-                <li><i className="material-icons">exit_to_app</i><a href="/signin"> Log Out</a></li>
+                <li><i className="material-icons">dashboard</i><Link to="/feed" > Dashboard</Link></li>
+                <li><i className="material-icons">supervisor_account</i><Link to="/users" > Connect</Link></li>
+                <li><i className="material-icons">chat</i><Link to="/chat-room"> Chat Rooms</Link></li>
+                <li><i className="material-icons">work</i><Link to="/job-connect"> Job Alert</Link></li>
+                <li><i className="material-icons">event_note</i><Link to="/events"> Events</Link></li>
+                <li><i className="material-icons">settings</i><Link to="/profile"> Profile</Link></li>
+                <li><i className="material-icons">exit_to_app</i><Link to="/signin"> Log Out</Link></li>
             </ul>
             
         </div> 
